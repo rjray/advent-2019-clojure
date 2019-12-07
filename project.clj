0effc20e933bf5c03749a82651818b12676e1d75
Randy J. Ray <rjray@blackperl.com>
2019-12-06 23:38:02 -0800

    Added clojure.math.combinatorics.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins [[lein-kibit "0.1.6"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.combinatorics "0.1.6"]])
